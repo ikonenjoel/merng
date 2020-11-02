eaf3cd75562c33f38720f8a55067c74d073f5355
ikonenjoel <drakening@gmail.com>
2020-11-02 14:39:40 +0200

    added authentication check and comment adding/deleting

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,19 @@
+const posts = require('./posts.js');
 const postsResolvers = require('./posts.js');
 const usersResolvers = require('./users.js');
+const commentsResolvers = require('./comments.js');
 
 module.exports= {
-    Query: {
-        ...postsResolvers.Query
-    },
-    Mutation: {
-        ...usersResolvers.Mutation
-    }
+  Post: {
+    likeCount: (parent) => parent.comments.length,
+    commentCount: (parent) => parent.comments.length
+  },
+  Query: {
+    ...postsResolvers.Query
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation
+  }
 };
