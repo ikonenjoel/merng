7a1036a3a79a3b1abda764207945083648b9037c
ikonenjoel <drakening@gmail.com>
2020-11-10 20:34:06 +0200

    added some custom css to center text.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { useQuery } from '@apollo/client';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Transition } from 'semantic-ui-react';
 import { AuthContext } from '../context/auth'
 
 import PostCard from '../components/PostCard'
@@ -23,14 +23,16 @@ export default function Home() {
         </Grid.Column>
       )}
       { loading ? (
-        <h1>Loading posts..</h1>
+        <h1 style={{display: "block", textAlign: "center", width: "100%"}}>Loading posts..</h1>
       ) : (
-        posts && posts.map((post => (
+        <Transition.Group>
+          {posts && posts.map((post) => (
           <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
             <PostCard post={post}/>
           </Grid.Column>
-        ))
-      ))}
+          ))}
+        </Transition.Group>
+      )}
     </Grid.Row>
     </Grid>
   )
