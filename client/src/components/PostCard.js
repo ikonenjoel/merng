f8e7e2edfce6ea1901a625cd40c477c3f71c8111
ikonenjoel <drakening@gmail.com>
2020-11-20 21:12:21 +0200

    added DeleteButton, singlepost page, fixed typos.

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 import { AuthContext } from '../context/auth'
 import LikeButton from './LikeButton'
+import DeleteButton from './DeleteButton'
 
 export default function PostCard({
    post: { body, createdAt, id, username, likeCount, commentCount, likes }
@@ -34,11 +35,7 @@ export default function PostCard({
             {commentCount}
           </Label>
         </Button>
-        {user && user.username === username && (
-          <Button as="div" color="red" floated="right" onClick={() => console.log('Delete Post')}>
-            <Icon name="trash" style={{ margin: 0 }}/>
-          </Button>
-        )} 
+        {user && user.username === username && <DeleteButton postId={id} />} 
       </Card.Content>
     </Card>
   )
---
+++ client/src/pages/SinglePost.js
@@ -0,0 +1,92 @@
+import React, { useContext } from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client'
+import { FETCH_POSTS_QUERY } from '../util/graphql';
+import { Card, Grid, Button, Icon, Label, Image } from 'semantic-ui-react';
+import moment from 'moment';
+
+import { AuthContext } from '../context/auth';
+import LikeButton from '../components/LikeButton';
+import DeleteButton from '../components/DeleteButton';
+
+export default function SinglePost(props){
+  const postId = props.match.params.postId;
+  const { user } = useContext(AuthContext);
+  console.log(postId);
+
+  const { data: { getPost } = {}} = useQuery(FETCH_POST_QUERY, {
+    variables: {
+      postId
+    }
+  })
+
+  function deletePostCallback(){
+    props.history.push('/')
+  }
+
+  let postMarkup;
+  if(!getPost){
+    postMarkup = <p>Loading post..</p>
+  } else {
+    const { id, body, createdAt, username, comments, likes, likeCount, commentCount } = getPost;
+
+    postMarkup = (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/molly.png' size="small" float="right" />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>{body}</Card.Description>
+              </Card.Content>
+              <hr/>
+              <Card.Content extra>
+                <LikeButton user={user} post={{ id, likeCount, likes }} />
+                <Button as="div" labelPosition="right"onClick={() => console.log('Comment on post')}>
+                  <Button basic color="blue">
+                    <Icon name="comment"/>
+                  </Button>
+                  <Label basic color="blue" pointing="left">
+                    {commentCount}
+                  </Label>
+                </Button>
+                {user && user.username === username && (
+                  <DeleteButton postId={id} callback={deletePostCallback} />
+                )}
+              </Card.Content>
+            </Card>
+            {comments.map(comment => (
+              <Card fluid key={comment.id}>
+                <Card.Content>
+                  <Card.Header>{comment.username}</Card.Header>
+                  <Card.Meta>{moment(comment.createdAt).fromNow()}</Card.Meta>
+                  <Card.Description>{comment.body}</Card.Description>
+                </Card.Content>
+              </Card>
+            ))}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    )
+  }
+  return postMarkup;
+}
+
+const FETCH_POST_QUERY = gql`
+  query($postId: ID!){
+    getPost(postId: $postId){
+      id body createdAt username likeCount
+      likes{
+        username
+      }
+      commentCount
+      comments{
+        id username createdAt body
+      }
+    }
+  }
+`
